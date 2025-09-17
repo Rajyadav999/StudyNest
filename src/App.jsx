ca0953e0e603325c766c6d92829d3bb9bda86a55
Rajyadav999 <yraj43801@gmail.com>
2025-09-17 08:33:38 +0530

    feat: add responsive navbar and hero background image
    - Implemented a navigation bar with links to Home, Program, About Us, Campus, and Testimonials
    - Added a hero section with a full-width background image for better visual appeal
    - Enhanced layout consistency and improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
 import './App.css'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Hero from   './Components/Hero/Hero.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
+    <Hero/>
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+import React from 'react'
+import logo from '../../assets/logo.png'
+
+function Navbar () {
+    return (
+    <nav className="container">
+      <img src = {logo}  alt="" className="logo"/>
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className="btn">Contact Us</button></li>
+        </ul>
+    </nav>
+    );
+}
+export default Navbar;
