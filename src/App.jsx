9dfe09003bf31e10798a14d5e7aabb029506f2b6
Rajyadav999 <yraj43801@gmail.com>
2025-09-18 17:43:15 +0530

    feat: add program and about university sections below hero
    - Introduced "What We Offer" section showcasing student graduation images
    - Added "About University" section with heading, description, and media support
    - Improved page flow by extending content after hero section
    - Enhanced structure for better presentation of programs and values#

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar.jsx'
 import Hero from   './Components/Hero/Hero.jsx'
-
-
+import Program from './Components/Program/Program.jsx'
+import Title from './Components/Title/Title.jsx'
+import About from './Components/About/About.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +12,12 @@ function App() {
     <>
     <Navbar/>
     <Hero/>
+    <div className ="container">
+      <Title subTitle="Our Program" Title="What We Offer"/>
+      <Program/>
+      <About/>
+       <Title subTitle="Gallery" Title="Campus Photo"/>
+      </div>
     </>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,33 @@
-import './Navbar.css'
-import React from 'react'
-import logo from '../../assets/logo.png'
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 
-function Navbar () {
-    return (
-    <nav className="container">
-      <img src = {logo}  alt="" className="logo"/>
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About Us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className="btn">Contact Us</button></li>
-        </ul>
+function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setSticky(window.scrollY > 500);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className="btn">Contact Us</button></li>
+      </ul>
     </nav>
-    );
+  );
 }
+
 export default Navbar;
